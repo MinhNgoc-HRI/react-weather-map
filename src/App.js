33bd86c732da3d19630cbb9c2990b1aa0b10d822
ngnm1009 <ngnm1009@gmail.com>
2021-08-15 17:46:55 +0700

    add lazy load

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 //REACT
-
+import React,{Suspense} from 'react';
 // ANTD vì lười :))
 import 'antd/dist/antd.css'; 
 //CONTEXT
 import WeatherProvider from "./store/WeatherProvider";
 //Component
-import SearchWeather from "./component/SearchWeather";
-import DetailWeather from "./component/DetailWeather";
+import Loading from './ui/Loading';
 //CSS
 import "./App.css";
+//LAZY COMPONENT
+const SearchWeather = React.lazy(()=> import('./component/SearchWeather'));
+const DetailWeather = React.lazy(()=> import("./component/DetailWeather"));
+
+// import SearchWeather from "./component/SearchWeather";
+// import DetailWeather from "./component/DetailWeather";
+
 
 function App() {
   return (
@@ -16,10 +22,15 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="col col-xl-3 p-4 bg-white">
+            <Suspense fallback={<Loading />}>
             <SearchWeather />
+            </Suspense>
           </div>
           <div className="col col-xl-9 p-4 bg-w h-90-vh">
-            <DetailWeather />
+          <Suspense fallback={<Loading />}>
+          <DetailWeather />
+            </Suspense>
+           
           </div>
         </div>
       </div>
